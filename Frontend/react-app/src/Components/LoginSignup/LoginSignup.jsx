e91a0903647cf2bfae61c43aba206d29a029e929
Vivek Aher <vivek4879gmail.com>
2025-02-09 02:30:04 -0500

    added login and signup page

---
+++ Frontend/react-app/src/Components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,42 @@
+import './LoginSignup.css'
+import {useState} from "react";
+
+const LoginSignup = () => {
+    const [action,setAction] = useState("Login");
+    return (
+
+        <div className="LoginSignup">
+            <div className="total-box">
+                <h1 className="Page-Heading">
+                    Gambare!
+                </h1>
+            </div>
+            <div className="header">
+                <div className="text">{action}</div>
+            </div>
+            <div className="inputs">
+                {action==="Login"?<div></div>:   <>            <div className="input">
+                    <input type="text" placeholder="First Name" required="true" />
+                </div>
+                    <div className="input">
+                        <input type="text" placeholder = "Last Name" required="true" />
+                    </div>
+                </>}
+                <div className="input">
+                    <input type="email" placeholder="Email" required="true" />
+                </div>
+                <div className="input">
+                    <input type="password" placeholder="Password" required="true" />
+                </div>
+            </div>
+            {action==="Login"?<div className="forgot-password">Forgot Password </div>: <div></div>}
+
+            <div className="submit-container">
+                <div className={action==="Login"?"submit-button gray":"submit-button"}onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+                <div className={action==="Sign Up"?"submit-button gray":"submit-button"}onClick={()=>{setAction("Login")}}>Login</div>
+            </div>
+        </div>
+    );
+};
+
+export default LoginSignup;
