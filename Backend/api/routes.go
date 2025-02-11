27b937262d4f5e447bc2ea6566dbb60227d576f2
Vivek Aher <vivek4879gmail.com>
2025-02-10 21:13:12 -0500

    added delete user

--- Backend/api/routes.go
+++ Backend/api/routes.go
@@ -51,6 +51,7 @@ func (app *application) logoutHandler(w http.ResponseWriter, r *http.Request) {
 	//http.Error(w, "Unauthorized:Session expired", http.StatusUnauthorized)
 
 	fmt.Println("User successfully logged out")
+
 	return
 }
 
@@ -67,12 +68,14 @@ func (app *application) signupHandler(w http.ResponseWriter, r *http.Request) {
 	_, err1 := app.Models.UserModel.Query(input.Email)
 	if err1 == nil {
 		fmt.Printf("%s already exists\n", input.Email)
+		http.Error(w, "Email already exists", http.StatusBadRequest)
 		return
 	}
 
 	hashedPassword := hashing(input.Password)
 	err := app.Models.UserModel.Insert(input.FirstName, input.LastName, input.Email, hashedPassword)
 	fmt.Println("User created")
+
 	if err != nil {
 		fmt.Println("here")
 		fmt.Println(err)
@@ -94,6 +97,7 @@ func (app *application) loginHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println("User not registered, please Sign up and then login")
+		http.Error(w, err.Error(), http.StatusUnauthorized)
 		return
 	}
 	match, err := argon2id.ComparePasswordAndHash(input.Password, user.Password)
@@ -170,25 +174,31 @@ func (app *application) DashboardHandler(w http.ResponseWriter, r *http.Request)
 func (app *application) deleteHandler(w http.ResponseWriter, r *http.Request) {
 	sessionToken, err := app.CheckCookie(w, r)
 	if err != nil {
+		http.Error(w, "Unauthorized: Session not found", http.StatusUnauthorized)
 		return
 	}
 	session, err2 := app.Models.UserModel.QUERYSESSION(sessionToken)
 	if err2 != nil {
+		fmt.Println(err2)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 	user, err3 := app.Models.UserModel.QueryUserId(session.UserID)
 	if err3 != nil {
 		fmt.Println(err3)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 	err4 := app.Models.UserModel.DeleteUser(*user)
 	if err4 != nil {
 		fmt.Println(err4)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 	err5 := app.Models.UserModel.DeleteSession(*session)
 	if err5 != nil {
 		fmt.Println(err5)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 	fmt.Println("User successfully deleted")
