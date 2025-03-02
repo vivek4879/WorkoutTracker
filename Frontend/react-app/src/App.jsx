1e6d2b408deffb3f695aef70ae0ce0ba417cf599
anuragk8 <anuragk619.ak@gmail.com>
2025-03-02 14:49:26 -0500

    Testing if there are merge conflicts

--- Frontend/react-app/src/App.jsx
+++ Frontend/react-app/src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./Components/Login/Login.jsx";
 import SignUp from "./Components/SignUp/SignUp.jsx";
 import ForgotPass from "./Components/ForgotPass/ForgotPass.jsx";
 import Dashboard from "./Components/Dashboard/Dashboard.jsx";
+import Test from "./Components/test/test.jsx";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPass />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/test" element={<Test />} />
           {/* Redirect the root path to the login page */}
           <Route path="/" element={<Navigate to="/login" replace />} />
           {/* Fallback route */}
