c7fab286376a6c4a6c6208fe543723a67f656df7
Rishitha <adepurishitha12@gmail.com>
2025-03-27 10:51:39 +0530

    added usergoals page

--- Frontend/react-app/src/App.jsx
+++ Frontend/react-app/src/App.jsx
@@ -10,6 +10,8 @@ import SignUp from "./Components/SignUp/SignUp.jsx";
 import ForgotPass from "./Components/ForgotPass/ForgotPass.jsx";
 import Dashboard from "./Components/Dashboard/Dashboard.jsx";
 import Profile from "./Components/UserProfile/Profile.jsx";
+import UserGoals from "./Components/UserGoals/usergoals.jsx";
+
 //  import Test from "./Components/test/test.jsx";
 
 function App() {
@@ -22,11 +24,14 @@ function App() {
           <Route path="/forgot-password" element={<ForgotPass />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/usergoals" element={<UserGoals/>} />
+
           {/* <Route path="/test" element={<Test />} /> */}
           {/* Redirect the root path to the login page */}
           <Route path="/" element={<Navigate to="/login" replace />} />
           {/* Fallback route */}
           <Route path="*" element={<Login />} />
+          
         </Routes>
       </Router>
     </div>
